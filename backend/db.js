55ae4e7da7d252c9636b4de9e37527ae77e49cba
ym1085 <youngmin1085@gmail.com>
2022-07-23 17:51:59 +0900

    MySQL 커넥션이 안되는 이슈가 존재

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'mysql',
     user: 'root',
-    password: '1234!',
-    database: 'myapp',
+    password: '!1234',
+    database: 'myapp;',
 });
 exports.pool = pool;
